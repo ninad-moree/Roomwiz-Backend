3ce95a9b22237402a711b2e7b959b6d44610132a
Ninad More <ninadmore183@gmail.com>
2024-09-30 10:38:42 +0530

    first commit

---
+++ src/main/java/com/hotels/roomwiz/RoomwizApplication.java
@@ -0,0 +1,13 @@
+package com.hotels.roomwiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomwizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomwizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotels/roomwiz/RoomwizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotels.roomwiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomwizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
