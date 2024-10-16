c6b7de60ff011be1b2c81c740d630b35e81d068b
Ninad More <ninadmore183@gmail.com>
2024-10-16 15:51:35 +0530

    Get mapping changed to Post Mapping

--- src/main/java/com/hotels/roomwiz/controller/BookingController.java
+++ src/main/java/com/hotels/roomwiz/controller/BookingController.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,7 @@ import com.hotels.roomwiz.service.IRoomService;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/bookings")
@@ -53,7 +56,7 @@ public class BookingController {
         }
     }
 
-    @GetMapping("/room/{roomId}/booking")
+    @PostMapping("/room/{roomId}/booking")
     public ResponseEntity<?> saveBookings(@PathVariable Long roomId, @RequestBody BookedRoom bookingRequest) {
         try {
             String confirmationCode = bookingService.saveBooking(roomId, bookingRequest);
