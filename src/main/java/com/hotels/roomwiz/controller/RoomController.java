a9079882b5c6ba91dea2f1b857d8369e6f26c1ef
Ninad More <ninadmore183@gmail.com>
2024-10-21 11:54:10 +0530

    Available rooms API created

--- src/main/java/com/hotels/roomwiz/controller/RoomController.java
+++ src/main/java/com/hotels/roomwiz/controller/RoomController.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import javax.sql.rowset.serial.SerialBlob;
 
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -100,9 +102,28 @@ public class RoomController {
         }).orElseThrow(() -> new ResourceNotFoundException("Room Not Found."));
     }
 
-    
+    @GetMapping("/available-rooms")
+    public ResponseEntity<List<RoomResponse>> getAvailableRooms(@RequestParam("checkInDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkInDate, @RequestParam("checkOutDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkOutDate, @RequestParam("roomType") String roomType) throws SQLException {
+        List<Room> availableRooms = roomService.getAvailableRooms(checkInDate, checkOutDate, roomType);
+        List<RoomResponse> roomResponses = new ArrayList<>();
+        for(Room room : availableRooms) {
+            byte[] photoBytes = roomService.getRoomPhotoByRoomId(room.getId());
+            if(photoBytes != null && photoBytes.length > 0) {
+                String photoBase64 = Base64.encodeBase64String(photoBytes);
+                RoomResponse roomResponse = getRoomResponse(room);
+                roomResponse.setPhoto(photoBase64);
+                roomResponses.add(roomResponse);
+            }
+        }
 
+        if(roomResponses.isEmpty()) {
+            return ResponseEntity.noContent().build(); 
+        } else {
+            return ResponseEntity.ok(roomResponses);
+        }
+    }
 
+    /* PRIVATE METHODS */
 
     private RoomResponse getRoomResponse(Room room) {
         // List<BookedRoom> bookings = getAllBookingsByRoomId(room.getId());
--- src/main/java/com/hotels/roomwiz/repository/RoomRepository.java
+++ src/main/java/com/hotels/roomwiz/repository/RoomRepository.java
@@ -1,5 +1,6 @@
 package com.hotels.roomwiz.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,9 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
 
     @Query("select distinct r.roomType from Room r")
     List<String> findDistinctRoomTypes();
+
+    @Query("select r from Room r " + "where r.roomType like %:roomType%" + "and r.id not in (" + " select br.room.id from BookedRoom br " + 
+            " where ((br.checkInDate <= :checkOutDate) and (br.checkOutDate >= :checkInDate))" + ")"
+    )
+    List<Room> findAvailableRoomsByDatesAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }
