2e7413c550b29605b0ffe24d6037c5acc7d5714f
Ninad More <ninadmore183@gmail.com>
2024-10-24 11:36:31 +0530

    User register API created

---
+++ src/main/java/com/hotels/roomwiz/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.hotels.roomwiz.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.hotels.roomwiz.exception.UserAlreadyExistsException;
+import com.hotels.roomwiz.model.User;
+import com.hotels.roomwiz.service.IUserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AuthController {
+    private final IUserService userService;
+
+    @PostMapping("/register-user")
+    public ResponseEntity<?> registerUser(User user) {
+        try {
+            userService.registeredUser(user);
+            return ResponseEntity.ok("user registered successfully");
+        } catch (UserAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+        }
+    }
+}
